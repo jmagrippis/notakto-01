7204249ff28950be26d0817afec6b7e1d83b1e0c
Johnny Magrippis <j@magrippis.com>
2018-12-13 12:49:43 +0000

    draft presentation

--- src/presentation.js
+++ src/presentation.js
@@ -1,39 +1,43 @@
-// Import React
-import React from 'react';
+import React from 'react'
 
-// Import Spectacle Core tags
 import {
+  Appear,
   BlockQuote,
   Cite,
+  CodePane,
   Deck,
   Heading,
-  ListItem,
+  Image,
+  Link,
   List,
+  ListItem,
   Quote,
   Slide,
   Text,
-} from 'spectacle';
+} from 'spectacle'
 
 // Import theme
-import createTheme from 'spectacle/lib/themes/default';
+import createTheme from 'spectacle/lib/themes/default'
 
 // Require CSS
-require('normalize.css');
+require('normalize.css')
 
 const theme = createTheme(
   {
-    primary: 'white',
-    secondary: '#1F2022',
-    tertiary: '#03A9FC',
-    quaternary: '#CECECE',
+    primary: '#DADFF7',
+    secondary: '#454545',
+    tertiary: '#61E786',
+    quaternary: '#EDFFEC',
   },
   {
     primary: 'Montserrat',
     secondary: 'Helvetica',
   }
-);
+)
 
-export default class Presentation extends React.Component {
+const sandboxUrl = 'https://codesandbox.io/s/40k4y7p364'
+
+export default class Presentation extends React.PureComponent {
   render() {
     return (
       <Deck
@@ -41,55 +45,281 @@ export default class Presentation extends React.Component {
         transitionDuration={500}
         theme={theme}
       >
-        <Slide transition={['zoom']} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+        <Slide transition={['zoom']} bgColor="tertiary">
+          <Heading size={1} fit caps lineHeight={1} textColor="quaternary">
+            notakto
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+          <Text margin="10px 0 0" textColor="quaternary" size={1} fit bold>
+            tic-tac-toe for grownups
           </Text>
         </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            Typography
-          </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
-          </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
-          </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
-          </Text>
-        </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+        <Slide transition={['fade']} bgColor="primary">
           <Heading size={6} textColor="secondary" caps>
-            Standard List
+            Keywords
           </Heading>
           <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
+            <ListItem>Typescript</ListItem>
+            <ListItem>Code Sandbox</ListItem>
+            <ListItem>React</ListItem>
           </List>
+        </Slide>
+        <Slide transition={['fade']} bgColor="primary">
+          <Heading size={6} textColor="secondary" caps>
+            Typescript
+          </Heading>
+          <List>
+            <ListItem>
+              Statically-typed transpiles-to-javascript language
+            </ListItem>
+            <ListItem>Makes your editor better at helping you</ListItem>
+            <ListItem>So hot right now</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={['fade']} bgColor="primary">
+          <Heading size={6} textColor="secondary" caps>
+            Code Sandbox
+          </Heading>
+          <List>
+            <ListItem>Powerful code editor that runs on your browser</ListItem>
+            <ListItem>
+              Imports external libraries you use, compiles your code, reloads
+              your preview
+            </ListItem>
+            <ListItem>
+              Useful for experiments and sharing code snippets
+            </ListItem>
+            <ListItem>
+              <Link href={sandboxUrl}>
+                Sandbox with the start of this project: {sandboxUrl}
+              </Link>
+            </ListItem>
+          </List>
+        </Slide>
+        <Slide transition={['fade']} bgColor="primary">
+          <Heading size={6} textColor="secondary" caps>
+            React
+          </Heading>
+          <List>
+            <ListItem>Revolutionary framework for User Interfaces</ListItem>
+            <ListItem>Has you thinking in Components</ListItem>
+            <ListItem>
+              Merges HTML, styling and javascript together on the component
+              level
+            </ListItem>
+          </List>
+        </Slide>
+        <Slide transition={['fade']} bgColor="primary">
+          <Heading size={6} textColor="secondary" caps>
+            Let's add some css!
+          </Heading>
+          <List>
+            <ListItem>
+              <Link href="https://coolors.co/app">
+                Coolors: generate appealing colour palettes
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link href="https://fonts.google.com/">
+                Google Fonts: publicly distributed professional fonts
+              </Link>
+            </ListItem>
+          </List>
+        </Slide>
+        <Slide transition={['fade']} bgColor="tertiary">
+          <Heading size={6} textColor="quaternary" caps>
+            Let's add some css!
+          </Heading>
+          <CodePane
+            lang="css"
+            textSize="1.3em"
+            source={require('./snippets/css1.txt')}
+            margin="20px auto"
+            overflow="overflow"
+          />
+        </Slide>
+        <Slide transition={['fade']} bgColor="tertiary">
+          <Heading size={6} textColor="quaternary" caps>
+            Let's add some css!
+          </Heading>
+          <CodePane
+            lang="html"
+            textSize="0.85em"
+            source={require('./snippets/html1.txt')}
+            margin="20px auto"
+            overflow="overflow"
+          />
+        </Slide>
+        <Slide transition={['fade']} bgColor="primary">
+          <Heading size={6} textColor="secondary" caps>
+            React Components
+          </Heading>
+          <List>
+            <ListItem>Class Components</ListItem>
+            <ListItem>Stateless Functional Components</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={['fade']} bgColor="tertiary">
+          <Heading size={6} textColor="quaternary" caps>
+            Class Components
+          </Heading>
+          <CodePane
+            lang="jsx"
+            textSize="0.85em"
+            source={require('./snippets/jsx1.txt')}
+            margin="20px auto"
+            overflow="overflow"
+          />
+        </Slide>
+        <Slide transition={['fade']} bgColor="tertiary">
+          <Heading size={6} textColor="quaternary" caps>
+            Let's add a Scoreboard section!
+          </Heading>
+          <Appear>
+            <div>
+              <CodePane
+                lang="jsx"
+                textSize="0.85em"
+                source={require('./snippets/jsx2.txt')}
+                margin="20px auto"
+                overflow="overflow"
+              />
+            </div>
+          </Appear>
+        </Slide>
+        <Slide transition={['fade']} bgColor="primary">
+          <Heading size={6} textColor="secondary" caps>
+            Props, State and Context
+          </Heading>
+          <List>
+            <Appear>
+              <ListItem>
+                Components can be "given" data as `props`. They cannot
+                manipulate `props` directly, they just use them.
+              </ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>
+                Components can have their own `state`. They can update their own
+                state as much as they want.
+              </ListItem>
+            </Appear>
+
+            <Appear>
+              <ListItem>
+                Components can use `context`, which is like `state` on steroids.
+              </ListItem>
+            </Appear>
+          </List>
+        </Slide>
+        <Slide transition={['fade']} bgColor="primary">
+          <Heading size={6} textColor="secondary" caps>
+            Props, State and Context
+          </Heading>
+          <List>
+            <ListItem>Components will almost always use `props`</ListItem>
+            <ListItem>Components will sometimes have `state`</ListItem>
+            <ListItem>
+              Components will only use `context` in advanced cases
+            </ListItem>
+          </List>
+        </Slide>
+        <Slide transition={['fade']} bgColor="tertiary">
+          <Heading size={6} textColor="quaternary" caps>
+            Let's create a Functional Component!
+          </Heading>
+          <Appear>
+            <div>
+              <CodePane
+                lang="jsx"
+                textSize="0.85em"
+                source={require('./snippets/jsx3.txt')}
+                margin="20px auto"
+                overflow="overflow"
+              />
+            </div>
+          </Appear>
+        </Slide>
+        <Slide transition={['fade']} bgColor="tertiary">
+          <Heading size={6} textColor="quaternary" caps>
+            Typescript interlude!
+          </Heading>
+          <CodePane
+            lang="ts"
+            textSize="1em"
+            source={require('./snippets/ts1.txt')}
+            margin="20px auto"
+            overflow="overflow"
+          />
+        </Slide>
+        <Slide transition={['fade']} bgColor="tertiary">
+          <Heading size={6} textColor="quaternary" caps>
+            Typescript interlude!
+          </Heading>
+          <CodePane
+            lang="ts"
+            textSize="0.85em"
+            source={require('./snippets/ts2.txt')}
+            margin="20px auto"
+            overflow="overflow"
+          />
+        </Slide>
+        <Slide transition={['fade']} bgColor="tertiary">
+          <Heading size={6} textColor="quaternary" caps>
+            Let's use some Props!
+          </Heading>
+          <CodePane
+            lang="jsx"
+            textSize="1em"
+            source={require('./snippets/jsx4.txt')}
+            margin="20px auto"
+            overflow="overflow"
+          />
+        </Slide>
+        <Slide transition={['fade']} bgColor="tertiary">
+          <Heading size={6} textColor="quaternary" caps>
+            Let's pass some Props!
+          </Heading>
+          <CodePane
+            lang="jsx"
+            textSize="0.9em"
+            source={require('./snippets/jsx5.txt')}
+            margin="20px auto"
+            overflow="overflow"
+          />
+        </Slide>
+        <Slide transition={['fade']} bgColor="tertiary">
+          <Heading size={6} textColor="quaternary" caps>
+            Let's use some State!
+          </Heading>
+          <CodePane
+            lang="jsx"
+            textSize="0.7em"
+            source={require('./snippets/jsx6.txt')}
+            margin="20px auto"
+            overflow="overflow"
+          />
+        </Slide>
+        <Slide transition={['fade']} bgColor="primary">
+          <Heading size={6} textColor="secondary" caps>
+            User interaction
+          </Heading>
+          <Appear>
+            <Image
+              margin="20px auto"
+              src={require('./images/we-have-technology.gif')}
+            />
+          </Appear>
+          <Text>Change the page according to user clicks!</Text>
         </Slide>
         <Slide transition={['fade']} bgColor="secondary" textColor="primary">
           <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
+            <Quote>Everything is possible, given infinite time</Quote>
+            <Appear>
+              <Cite>Johnny</Cite>
+            </Appear>
           </BlockQuote>
         </Slide>
       </Deck>
-    );
+    )
   }
 }
